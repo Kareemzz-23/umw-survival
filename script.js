577810ddb159054b20100bdc08c1ca7ce69192fa
Kareemzz-23 <trashkareem@gmail.com>
2025-01-03 14:41:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
+const cardWrapper = document.querySelector('.card-wrapper');
+
+leftBtn.addEventListener('click', () => {
+    cardWrapper.scrollBy({
+        left: -300,
+        behavior: 'smooth'
+    });
+});
+
+rightBtn.addEventListener('click', () => {
+    cardWrapper.scrollBy({
+        left: 300,
+        behavior: 'smooth'
+    });
+});
